5352a8408d1003b47916b3d5efeb07de68770119
BenjaminECohen <cohenb@ufl.edu>
2020-01-16 15:36:44 -0500

    Added in level select and version 3 of the game manager

---
+++ LevelSelect.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelect : MonoBehaviour
+{
+    [Tooltip("Enter levels (GameObject plots) in order of game flow")]
+    public GameObject[] level;
+    private int levelIndex;
+
+    /*===========================================================
+     * To Do With This Script and It's Implementation
+     * 1. In the game manager, have functions that highlight the plot with another color
+     * 2. Have the game manager save the current plot and enter it into a camera script that will "Look At" the current plot
+     * ===========================================================
+     */
+
+    // This function is to be called in the Game Manager Start Function 
+    public GameObject firstLevel()
+    {
+        //Debug.Log("Initialized Level " + (levelIndex + 1));
+        return level[levelIndex];
+    }
+
+
+    //This function is to be called after a successfull building placement
+    public GameObject nextLevel()
+    {
+        levelIndex++;
+        //Debug.Log("Level " + (levelIndex) + " finished.");
+        if (levelIndex >= level.Length)
+        {
+            Debug.Log("All Levels Complete");
+            return null;
+        }
+        //Debug.Log("Level " + (levelIndex + 1) + " started.");
+        return level[levelIndex];
+    }
+}
---
+++ V3_GameManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class V3_GameManager : MonoBehaviour
+{
+    /* =========================================================
+     * MISSING PARTS THAT NEED TO BE IMPLEMENTED BY ORDER OF IMPORTANCE
+     * =========================================================
+     * 1. Condition Checking needs to be implemented
+     * 2. Camera motion handling
+     * 3. Responses to succeed/fail of building placement
+     * 4. Might want to make a separate UI handling script that is called to handle all UI interactions and events
+     * 5. Twitter functionality should be good by needs to be looked over
+     */
+    
+    private GameObject currLvl;
+
+
+    private class LevelVariables
+    {
+        public Color originalColor;
+    }
+    private LevelVariables lvlVars;
+    public Color highlightColor;
+
+    [Tooltip("Used for twitter functionality")]
+    public Screencapture snapCam;      //Use snapCam.CallTakeSnapshot() to use the snapshot feature
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveToLevel(this.GetComponent<LevelSelect>().firstLevel());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    //Highlight current plot/level
+    public void highlightLevel(GameObject level)
+    {
+        lvlVars.originalColor = level.GetComponent<Renderer>().material.color;
+        level.GetComponent<Renderer>().material.color = highlightColor;
+    }
+
+    public void unhighlightLevel(GameObject level)
+    {
+        level.GetComponent<Renderer>().material.color = lvlVars.originalColor;
+    }
+
+    //Switches highlight to the next level
+    public void moveToLevel(GameObject level)
+    {
+        currLvl = level;
+        highlightLevel(currLvl);
+    }
+
+}
