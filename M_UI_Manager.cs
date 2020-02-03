522bf700dfc7c97873e9174ec2e5754cf3d4ddfc
Michael O'Connell <michaeljoconnell@ufl.edu>
2020-02-03 16:19:31 -0500

    added temp ui manager

---
+++ M_UI_Manager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class M_UI_Manager : MonoBehaviour
+{
+    public GameObject UI_Object;
+    public Text message;
+    public Text header;
+
+    public void Enable_UI(bool activate) 
+    {
+        UI_Object.SetActive(activate);
+    }
+
+    public void EditMessage(string header, string message)
+    {
+        this.header.text = header;
+        this.message.text = message;
+        Enable_UI(true);
+    }
+
+}
